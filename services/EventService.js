747db9442e9059493d8e6d53e26d31ad0087e3c1
David-Tsui <st880221@gmail.com>
2021-04-25 23:49:57 +0800

    fix(deploy):  修正部署問題

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:7000`,
+  baseURL: `https://my-json-server.typicode.com/David-Tsui/nuxt-starter`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
